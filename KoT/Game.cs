e0db413687a704988125b0decdfec56a3e4de37e
Kc314 <kylec.314@gmail.com>
2018-02-21 14:52:24 -0800

    init setup

--- KoT/Game.cs
+++ KoT/Game.cs
@@ -14,13 +14,29 @@ namespace KoT
         }
         bool Round()
         {
-
+            for(int i = 0; i < PlayerCount; i++)
+            {
+                unwin = Move(Players[i]);
+                if(PlayerCount - dead == 1)
+                {
+                    winner = //the alive one
+                    return false;
+                } else if(Players[i].points >= 20)
+                {
+                    winner = Players[i];
+                    return false;
+                }
+            }
+        }
+        bool Move(Player P)
+        {
             return true;
         }
 
-
-
-
+        Player winner;
+        bool unwin;
+        int dead;
+        int PlayerCount;
         Player[] Players;
     }
 }
