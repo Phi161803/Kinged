fe609e1ceb5788877c0083ac6a594085b61fc2fd
Kc314 <kylec.314@gmail.com>
2018-02-21 14:48:07 -0800

    initial repo

---
+++ KoT/Game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoT
+{
+    class Game
+    {
+        Game()
+        {
+
+        }
+        bool Round()
+        {
+
+            return true;
+        }
+
+
+
+
+        Player[] Players;
+    }
+}
